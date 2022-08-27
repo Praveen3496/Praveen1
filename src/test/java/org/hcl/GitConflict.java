e028f0764c0262032c5745e4bed219d58b086aa1
Praveen3496 <hpraveen30496@gmail.com>
2022-08-27 12:38:13 +0530

    message1

---
+++ src/test/java/org/hcl/GitConflict.java
@@ -0,0 +1,13 @@
+package org.hcl;
+
+public class GitConflict {
+private void method1() {
+System.out.println("Method1");
+}
+private void method2() {
+	System.out.println("Method2");
+}
+private void method3() {
+	System.out.println("Method3");
+}
+}
